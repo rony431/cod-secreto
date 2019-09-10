505ce825ab3610e68f8df5a89db6c161dbf0a599
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-09-09 19:36:08 -0500

    express fixed

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+  var name = req.query.nombre;
+  if (name){
+    res.send('<h1>Hola '+name+'!</h1>');
+  }else{
+    res.send('<h1>Hola desconocido!</h1>');
+  }
+
+
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
