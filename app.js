0406fffa2331261879a96d88cafb137f1ed8ffdd
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-09-10 18:03:16 -0500

    fixing error

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
+  var str = ''
   for(var i = 1; i<=50; i++){
     console.log(i)
     if (i % 2 == 0){
-      res.write('<p> '+i+'Soy Par!</p>');
+      str +='<p>'+i+' Soy Par!</p>'
     }else{
-      res.write('<p> '+i+'Soy Impar!</p>');
+      str +='<p>'+i+' Soy Impar!</p>'
     }
-
   }
-
+  res.send(str)
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
