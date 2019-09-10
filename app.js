042c9ee652c5d33503da2eec15db03fa318377e3
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-09-09 20:08:09 -0500

    parameters

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  var name = req.query.nombre;
+app.get('/makers/:nombre', (req, res) => {
+  var name = req.params.nombre;
   if (name){
-    res.send('<h1>Hola '+name+'!</h1>');
+    res.send('<h1>Hola '+stringCap(name)+'!</h1>');
   }else{
     res.send('<h1>Hola desconocido!</h1>');
   }
 
 
 });
-
+function stringCap(name) 
+{
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
 app.listen(3000, () => console.log('Listening on port 3000!'));
