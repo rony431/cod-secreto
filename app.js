e4b3344251c0ade5ea8a102eb01f8f1ba13c0ad8
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-09-10 20:16:40 -0500

    header

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var app = express();
-var useragent = require('express-useragent');
- 
-app.use(useragent.express());
-app.get('/result', function(req, res){
-    res.send(req.useragent.browser);
-});
 
+app.get('/result', function(req, res){
+    res.send(req.header('user-agent'));
+});
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
