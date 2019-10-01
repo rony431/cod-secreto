089c60ed34394537a0484fb736475971e36bc0ea
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-09-30 19:24:30 -0500

    fixxxi

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.get('/', function(req, res){
     res.send(req.header('user-agent'));
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(process.env.PORT || 3000 , () => console.log('Listening on port 3000!'));
